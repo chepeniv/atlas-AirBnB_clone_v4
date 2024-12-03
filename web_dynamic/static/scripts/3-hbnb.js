1bee6e3e3d76ba44f0821ff7aff7f8a60a0ac599
Nash Thames <taylor.thames@atlasschool.com>
2024-12-03 03:52:13 -0600

    work on fetching Places - POST request, loop

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -27,7 +27,42 @@ $(function () {
     }
   });
 
-  $.get('http://localhost:5001/api/v1/places_search/', function (data, stat) {
+
+  $.ajax({
+    url: 'http://localhost:5001/api/v1/places_search',
+    type: 'POST',
+    contentType: 'application/json',
+    data: '{}', // empty dict
+    success: function (place) {
+      $('section.places').empty(); // clear the section
+
+      for (const place of places) { // loop through the places
+        const htmlPlaces = `
+        <article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">$${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+            <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+          </div>
+          <div class="description">
+            ${place.description}
+          </div>
+        </article>`;
+        $('section.places').append(htmlPlaces); // append the article to the section
+      }
+    },
+    error: function (error) {
+      console.error('Error in fetching places:', error);
+    }
+
+
+      
+
+
     // send POST with content-type: application/json with an empty dict in
     // the body :
     // curl "http://0.0.0.0:5001/api/v1/places_search" -XPOST -H "Content-Type: application/json" -d '{}'`

f297eef48ebc2e09bbb59b8a3d458373fc931777
Nash Thames <taylor.thames@atlasschool.com>
2024-12-03 06:36:34 -0600

    button clicking

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -36,7 +36,7 @@ $(function () {
       $('section.places').empty(); // clear the section
 
       for (const place of places) { // loop through the places
-        const htmlPlaces = `
+        const articlePlaces = `
         <article>
           <div class="title_box">
             <h2>${place.name}</h2>
@@ -51,17 +51,51 @@ $(function () {
             ${place.description}
           </div>
         </article>`;
-        $('section.places').append(htmlPlaces); // append the article to the section
+        $('section.places').append(articlePlaces); // append the article to the section
       }
     },
     error: function (error) {
-      console.error('Error in fetching places:', error);
+      console.error('Error fetching places:', error);
     }
-    // send POST with content-type: application/json with an empty dict in
-    // the body :
-    // curl "http://0.0.0.0:5001/api/v1/places_search" -XPOST -H "Content-Type: application/json" -d '{}'`
-    // loop through the request result and create an article in section.places
-    // for each place extracted
-    // owner tag in the place description may be removed
+  });
+
+  $('button').click(function () { // button click
+    const checkedAmenities = [];
+    $('div.amenities input[type="checkbox"]').each(function () {
+      if ($(this).is(':checked')) { // if checked
+        checkedAmenities.push($(this).data('id')); // push that id
+      }
+    });
+  
+    $.ajax({
+      url: 'http://localhost:5001/api/v1/places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ amenities: checkedAmenities }),
+      success: function (placesResponse) {
+        $('section.places').empty();
+        for (const place of placesResponse) {
+          const articlePlaces = `
+          <article>
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">$${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+              <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+              <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+            </div>
+            <div class="description">
+              ${place.description}
+            </div>
+          </article>`;
+          $('section.places').append(articlePlaces);
+        }
+      },
+      error: function (error) {
+        console.error('Error fetching places:', error);
+      }
+    });
   });
 });

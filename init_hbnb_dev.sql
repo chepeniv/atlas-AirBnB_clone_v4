1629e0f2a9422b37e141cba6e2a0f13adaf09810
chepeniv <jose.olmos@atlasschool.com>
2024-12-03 01:16:33 -0600

    created sh script to reset, reconstruct, and populate hbnb_dev_db

--- init_hbnb_dev.sql
+++ init_hbnb_dev.sql
@@ -1,14 +1,14 @@
--- create database if it doesn't exist
+DROP DATABASE IF EXISTS hbnb_dev_db;
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- create user if it doesn't exist attatched with password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- grant all privileges only on hbnb_dev_db
+-- SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- grant select privileges only on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
--- push privileges
 FLUSH PRIVILEGES;
+
---
+++ reset_hbnb_dev.sh
@@ -0,0 +1,11 @@
+# usage:
+# sudo sh reset_hbnb_dev.sh
+
+# reset database
+cat init_hbnb_dev.sql | mysql
+
+# start console for it to structure the database
+echo exit | sh start_console.sh
+
+# dump data into new database
+cat data/100-dump.sql | mysql

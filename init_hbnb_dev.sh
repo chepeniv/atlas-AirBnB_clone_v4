5c60e24bdb1de28fb09ecb9b92e3ac32f0d44931
chepeniv <jose.olmos@atlasschool.com>
2024-12-04 04:08:46 -0600

    organized, archivied, and renamed supplementary files

--- init_hbnb_dev.sh
+++ init_hbnb_dev.sh
@@ -2,10 +2,10 @@
 # sudo sh init_hbnb_dev.sh
 
 # reset database
-cat reset_hbnb_dev.sql | mysql
+cat data/reset_hbnb_dev.sql | mysql
 
 # start console for it to structure the database
-echo exit | sh start_console.sh
+echo exit | sh start_console.sh 2>error.log 1>/dev/null
 
 # dump data into new database
 cat data/100-dump.sql | mysql
